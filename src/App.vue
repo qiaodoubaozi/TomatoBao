7f1f7c490c1a59e894ac274087a8530c506b198c
qiaodoubaozi <19507482751>
2024-02-20 21:21:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup lang='ts'>
+// import { ref } from 'vue'
+let fun = () => 
+
+{
+  console.log('fun')
+}
+
+fun();
+
+
+</script>
+
+<style scoped>
+
+</style>
