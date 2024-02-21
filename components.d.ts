8b014d469a33d67ee9a5dcc7c2bf2a6235a2f0b5
qiaodoubaozi <19507482751>
2024-02-21 22:17:03 +0800

    feat: 初步设计主页面，绘制logo

--- components.d.ts
+++ components.d.ts
@@ -9,12 +9,16 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElAlert: typeof import('element-plus/es')['ElAlert']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCard: typeof import('element-plus/es')['ElCard']
+    ElCol: typeof import('element-plus/es')['ElCol']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElTimer: typeof import('element-plus/es')['ElTimer']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Timer: typeof import('./src/components/timer.vue')['default']
   }
 }
--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,14 +1,51 @@
 <template>
   <div>
-    <h2>homepage</h2>
+    <div class="container">
+      <div class="item-container">
+        <el-row>
+          <el-col :span="4" />
+          <el-col :span="16" class="timer-container">
+            <el-card></el-card>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="title-container">
+        <img src="@/assets/tomato_logo.png" alt="logo" style="height: 20vh" />
+        <h1 style="color: #ef4015; font-size: 2vh">===番茄包===</h1>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="content-container"></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 // import { ref } from "vue"
-
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.container {
+  height: 100vh;
+  background-color: #fff;
+  .item-container {
+    z-index: 1;
+    height: 80vh;
+    background-color: #fff;
+    position: sticky;
+    top: 0;
+  }
+  .title-container {
+    height: 20vh;
+    position: relative;
+    background-color: #ffe57f;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+.footer {
+  height: 1000px;
+  background-color: #ff0000;
+}
 </style>
